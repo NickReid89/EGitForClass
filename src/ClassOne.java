9333135253b662a1b94209cbda1339beaae2fe5d
test <test@test.com>
2018-04-08 20:45:23 -0300

    Init commit

---
+++ src/ClassOne.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nickolas
+ *
+ */
+public class ClassOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!");
+	}
+
+}
