bda369636232c67374cfffdd6082cb7d731c49b9
test test <test@test.com>
2018-04-08 21:07:18 -0300

    Revert "added println"
    This reverts commit 6c65dc4a30e700dea0ee60fe0f51ee41340e8f4c.

--- src/ClassOne.java
+++ src/ClassOne.java
@@ -14,8 +14,6 @@ public class ClassOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
-		
-		System.out.println("something else");
 	}
 
 }
